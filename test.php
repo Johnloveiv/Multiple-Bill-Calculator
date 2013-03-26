e7142987fc4e91fe47cedd04777bbb6e435f4a28
Judson Mitchell <judsonmitchell@gmail.com>
2013-03-26 05:39:22 -0500

    Remove unncessary files

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$a = array(
-array('id' =>'5','name' => 'Theft'),
-array('id' =>'15','name' => 'Theft 2nd Offense'),
-array('id' =>'20','name' => 'Theft 3rd Offense'),
-array('id' =>'25','name' => 'Arson'),
-);
-echo json_encode($a);
